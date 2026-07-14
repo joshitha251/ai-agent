3edc8c6dbba86f5665c58158fa473a4dcf17fb1d
Joshitha <joshitha1903@gmail.com>
2026-07-14 12:32:55 +0530

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from google import genai
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+
+@app.get("/")
+def home():
+    return {"message": "AI Sales Agent Backend Running 🚀"}
+
+
+@app.get("/test-ai")
+def test_ai():
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Say hello to Josh in one sentence."
+    )
+
+    return {
+        "response": response.text
+    }
